f1433c592654132eadcca67aab9cb1073bead853
gilades <gilades@post.jce.ac.il>
2012-12-16 11:58:42 +0200

    GREEN

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -11,6 +11,9 @@ public class RomanNumeralUnitTests {
 	public void testThat1ReturnsI() {
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
+		assertEquals("M", number.toRoman(1000));
+		assertEquals("XLIX",number.toRoman(49));
+		assertEquals("MM",number.toRoman(2000));
 	}
 	
 
